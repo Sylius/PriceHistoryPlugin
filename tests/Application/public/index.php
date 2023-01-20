e5b877534058de8a3ba9bf62d71cd5b8c8f79ef3
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2023-01-20 11:22:23 +0100

    Fix test app

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Tests\Sylius\PriceHistoryPlugin\Application\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/../../vendor/autoload_runtime.php';
+
+$_SERVER['APP_RUNTIME_OPTIONS'] = ['project_dir' => dirname(__DIR__)];
 
 return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
