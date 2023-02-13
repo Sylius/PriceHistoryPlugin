14518fd639c8e5b64968958f85d92bb67136a866
Rafikooo <thecrazyrafik@gmail.com>
2023-02-13 13:08:51 +0100

    [LowestPriceBeforeTheDiscount][API] Add serialization of lowestPriceBeforeDiscount field

---
+++ spec/Infrastructure/Serializer/ProductVariantNormalizerSpec.php
@@ -0,0 +1,165 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\PriceHistoryPlugin\Infrastructure\Serializer;
+
+use phpDocumentor\Reflection\Types\Context;
+use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\ApiBundle\SectionResolver\AdminApiSection;
+use Sylius\Bundle\ApiBundle\SectionResolver\ShopApiSection;
+use Sylius\Bundle\ApiBundle\Serializer\ContextKeys;
+use Sylius\Bundle\CoreBundle\SectionResolver\SectionProviderInterface;
+use Sylius\Component\Core\Exception\MissingChannelConfigurationException;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+use Sylius\PriceHistoryPlugin\Application\Provider\ProductVariantPriceProviderInterface;
+use Sylius\PriceHistoryPlugin\Domain\Model\ChannelInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class ProductVariantNormalizerSpec extends ObjectBehavior
+{
+    private const ALREADY_CALLED = 'sylius_price_history_product_variant_normalizer_already_called';
+
+    function let(ProductVariantPriceProviderInterface $priceProvider, SectionProviderInterface $sectionProvider): void
+    {
+        $this->beConstructedWith($priceProvider, $sectionProvider);
+    }
+
+    function it_supports_only_product_variant_interface(
+        ChannelInterface $channel,
+        OrderInterface $order,
+        ProductVariantInterface $variant,
+    ): void {
+        $this->supportsNormalization($variant, context: [ContextKeys::CHANNEL => $channel])->shouldReturn(true);
+        $this->supportsNormalization($order, context: [ContextKeys::CHANNEL => $channel])->shouldReturn(false);
+    }
+
+    function it_supports_normalization_if_section_is_shop_get(
+        SectionProviderInterface $sectionProvider,
+        ChannelInterface $channel,
+        ProductVariantInterface $variant,
+        ShopApiSection $shopApiSection,
+    ): void {
+        $sectionProvider->getSection()->willReturn($shopApiSection);
+        $this->supportsNormalization($variant, context: [ContextKeys::CHANNEL => $channel])->shouldReturn(true);
+    }
+
+    function it_does_not_support_if_section_is_admin_get(
+        SectionProviderInterface $sectionProvider,
+        AdminApiSection $adminApiSection,
+        ChannelInterface $channel,
+        ProductVariantInterface $variant,
+    ): void {
+        $sectionProvider->getSection()->willReturn($adminApiSection);
+        $this->supportsNormalization($variant, context: [ContextKeys::CHANNEL => $channel])->shouldReturn(false);
+    }
+
+    function it_does_not_support_if_the_normalizer_has_been_already_called(ProductVariantInterface $variant): void
+    {
+        $this
+            ->supportsNormalization($variant, null, [self::ALREADY_CALLED => true])
+            ->shouldReturn(false)
+        ;
+    }
+
+    function it_does_not_support_if_there_is_no_channel_in_the_context(ProductVariantInterface $variant): void
+    {
+        $this
+            ->supportsNormalization($variant)
+            ->shouldReturn(false)
+        ;
+    }
+
+    function it_adds_lowest_price_before_discount_to_variant_data(
+        ProductVariantPriceProviderInterface $priceProvider,
+        NormalizerInterface $normalizer,
+        ChannelInterface $channel,
+        ProductVariantInterface $variant,
+    ): void {
+        $this->setNormalizer($normalizer);
+
+        $context = [ContextKeys::CHANNEL => $channel];
+
+        $normalizer
+            ->normalize(
+                $variant,
+                null,
+                array_merge($context, [self::ALREADY_CALLED => true])
+            )
+            ->willReturn([])
+        ;
+
+        $priceProvider->getLowestPriceBeforeDiscount($variant, $channel)->willReturn(3700);
+
+        $this->normalize($variant, null, $context)->shouldBeLike(['lowestPriceBeforeDiscount' => 3700]);
+    }
+
+    function it_does_not_add_lowest_price_before_discount_to_variant_data_if_missing_channel_configuration_exception_is_thrown(
+        ProductVariantPriceProviderInterface $priceProvider,
+        NormalizerInterface $normalizer,
+        ChannelInterface $channel,
+        ProductVariantInterface $variant,
+    ): void {
+        $this->setNormalizer($normalizer);
+
+        $context = [ContextKeys::CHANNEL => $channel];
+
+        $normalizer
+            ->normalize(
+                $variant,
+                null,
+                array_merge($context, [self::ALREADY_CALLED => true])
+            )
+            ->willReturn([])
+        ;
+
+        $priceProvider
+            ->getLowestPriceBeforeDiscount($variant, $channel)
+            ->willThrow(MissingChannelConfigurationException::class)
+        ;
+
+        $this->normalize($variant, null, $context)->shouldBeLike([]);
+    }
+
+    function it_throws_an_exception_if_the_normalizer_has_been_already_called(
+        NormalizerInterface $normalizer,
+        ProductVariantInterface $variant,
+    ): void {
+        $this->setNormalizer($normalizer);
+
+        $normalizer->normalize($variant, null, [self::ALREADY_CALLED => true])->shouldNotBeCalled();
+
+        $this
+            ->shouldThrow(\InvalidArgumentException::class)
+            ->during('normalize', [$variant, null, [self::ALREADY_CALLED => true]])
+        ;
+    }
+
+    function it_throws_an_exception_if_there_is_no_channel_in_the_context(
+        NormalizerInterface $normalizer,
+        ProductVariantInterface $variant,
+    ): void {
+        $this->setNormalizer($normalizer);
+
+        $normalizer
+            ->normalize($variant, null, [self::ALREADY_CALLED => true])
+            ->willReturn([])
+        ;
+
+        $this
+            ->shouldThrow(\InvalidArgumentException::class)
+            ->during(
+                'normalize',
+                [$variant, null, [self::ALREADY_CALLED => false]]
+            )
+        ;
+    }
+}
---
+++ src/Infrastructure/Serializer/ProductVariantNormalizer.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\PriceHistoryPlugin\Infrastructure\Serializer;
+
+use Sylius\Bundle\ApiBundle\SectionResolver\AdminApiSection;
+use Sylius\Bundle\ApiBundle\Serializer\ContextKeys;
+use Sylius\Bundle\CoreBundle\SectionResolver\SectionProviderInterface;
+use Sylius\Component\Core\Exception\MissingChannelConfigurationException;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+use Sylius\PriceHistoryPlugin\Application\Provider\ProductVariantPriceProviderInterface;
+use Sylius\PriceHistoryPlugin\Domain\Model\ChannelInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Webmozart\Assert\Assert;
+
+final class ProductVariantNormalizer implements NormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    private const ALREADY_CALLED = 'sylius_price_history_product_variant_normalizer_already_called';
+
+    public function __construct(
+        private ProductVariantPriceProviderInterface $priceProvider,
+        private SectionProviderInterface $uriBasedSectionContext,
+    ) {
+    }
+
+    public function normalize(mixed $object, string $format = null, array $context = [])
+    {
+        Assert::isInstanceOf($object, ProductVariantInterface::class);
+        Assert::keyNotExists($context, self::ALREADY_CALLED);
+
+        $context[self::ALREADY_CALLED] = true;
+        $data = $this->normalizer->normalize($object, $format, $context);
+        Assert::isArray($data);
+
+        /** @var ChannelInterface $channel */
+        $channel = $context[ContextKeys::CHANNEL];
+
+        try {
+            $data['lowestPriceBeforeDiscount'] = $this->priceProvider->getLowestPriceBeforeDiscount($object, $channel);
+        } catch (MissingChannelConfigurationException) {
+        }
+
+        return $data;
+    }
+
+    public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool
+    {
+        if (isset($context[self::ALREADY_CALLED]) || !array_key_exists(ContextKeys::CHANNEL, $context)) {
+            return false;
+        }
+
+        return $data instanceof ProductVariantInterface && $this->isNotAdminApiSection();
+    }
+
+    private function isNotAdminApiSection(): bool
+    {
+        return !$this->uriBasedSectionContext->getSection() instanceof AdminApiSection;
+    }
+}
