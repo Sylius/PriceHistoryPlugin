8a3e4f128524db64effe37c42b87285712b2a9e9
Grzegorz Sadowski <sadowskigp@gmail.com>
2023-03-17 14:09:32 +0100

    [Maintenance] Adjust database table names to Sylius codebase

--- src/Domain/Model/LowestPriceForDiscountedProductsAwareTrait.php
+++ src/Domain/Model/LowestPriceForDiscountedProductsAwareTrait.php
@@ -32,13 +32,13 @@ trait LowestPriceForDiscountedProductsAwareTrait
      * @var Collection|TaxonInterface[]
      *
      * @ORM\ManyToMany(targetEntity="Sylius\Component\Taxonomy\Model\TaxonInterface")
-     * @ORM\JoinTable(name="sylius_price_history_channel_excluded_taxons",
+     * @ORM\JoinTable(name="sylius_channel_excluded_taxons",
      *     joinColumns={@ORM\JoinColumn(name="channel_id", referencedColumnName="id", onDelete="CASCADE")},
      *     inverseJoinColumns={@ORM\JoinColumn(name="taxon_id", referencedColumnName="id", onDelete="CASCADE")}
      * )
      */
     #[ORM\ManyToMany(targetEntity: TaxonInterface::class)]
-    #[ORM\JoinTable(name: 'sylius_price_history_channel_excluded_taxons')]
+    #[ORM\JoinTable(name: 'sylius_channel_excluded_taxons')]
     #[ORM\JoinColumn(name: 'channel_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
     #[ORM\InverseJoinColumn(name: 'taxon_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
     protected Collection $taxonsExcludedFromShowingLowestPrice;
--- src/Infrastructure/Migrations/Version20230126131430.php
+++ src/Infrastructure/Migrations/Version20230126131430.php
@@ -27,16 +27,16 @@ final class Version20230126131430 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE sylius_price_history_channel_pricing_log_entry (id INT AUTO_INCREMENT NOT NULL, channel_pricing_id INT NOT NULL, price INT NOT NULL, original_price INT DEFAULT NULL, logged_at DATETIME NOT NULL, INDEX IDX_B3F5AAC23EADFFE5 (channel_pricing_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE sylius_price_history_channel_pricing_log_entry ADD CONSTRAINT FK_B3F5AAC23EADFFE5 FOREIGN KEY (channel_pricing_id) REFERENCES sylius_channel_pricing (id) ON DELETE CASCADE');
-        $this->addSql('INSERT INTO `sylius_price_history_channel_pricing_log_entry` (`channel_pricing_id`, `price`, `original_price`, `logged_at`) SELECT `id`, `price`, `original_price`, NOW() FROM `sylius_channel_pricing`');
+        $this->addSql('CREATE TABLE sylius_channel_pricing_log_entry (id INT AUTO_INCREMENT NOT NULL, channel_pricing_id INT NOT NULL, price INT NOT NULL, original_price INT DEFAULT NULL, logged_at DATETIME NOT NULL, INDEX IDX_B3F5AAC23EADFFE5 (channel_pricing_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE sylius_channel_pricing_log_entry ADD CONSTRAINT FK_B3F5AAC23EADFFE5 FOREIGN KEY (channel_pricing_id) REFERENCES sylius_channel_pricing (id) ON DELETE CASCADE');
+        $this->addSql('INSERT INTO `sylius_channel_pricing_log_entry` (`channel_pricing_id`, `price`, `original_price`, `logged_at`) SELECT `id`, `price`, `original_price`, NOW() FROM `sylius_channel_pricing`');
     }
 
     public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_price_history_channel_pricing_log_entry DROP FOREIGN KEY FK_B3F5AAC23EADFFE5');
-        $this->addSql('DROP TABLE sylius_price_history_channel_pricing_log_entry');
+        $this->addSql('ALTER TABLE sylius_channel_pricing_log_entry DROP FOREIGN KEY FK_B3F5AAC23EADFFE5');
+        $this->addSql('DROP TABLE sylius_channel_pricing_log_entry');
     }
 }
